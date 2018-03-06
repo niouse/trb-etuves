37734860cc191f854d9daf1c5f375d19be65ac35
niouse <seba.pinard@gmail.com>
2018-03-06 16:08:26 +0100

    first commit

---
+++ imports/startup/server/fixtures.js
@@ -0,0 +1,4 @@
+import  "./../../api/users/server/users.fixitures.js";
+import  "./../../api/recorders/server/recorders.fixitures.js";
+import  "./../../api/series/server/series.fixitures.js";
+import  "./../../api/etuves/server/etuves.fixitures.js";
---
+++ server/main.js
@@ -0,0 +1,21 @@
+import { Meteor } from 'meteor/meteor';
+
+import './../imports/api/users/server';
+
+//trb-recorder
+import './../imports/api/recorders/server';
+import './../imports/api/series/server';
+import "./../imports/api/etuves/server"
+import './../imports/api/routes';
+
+
+import  './../imports/startup/server';
+
+
+Meteor.startup(() => {
+	loadUsers()
+    //trb-recorder
+    loadRecorders(16)
+    loadSeries()
+    loadEtuves()
+});
